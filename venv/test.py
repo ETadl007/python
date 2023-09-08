6aa491f530e76d5072906f2e4c847bed60d29e96
ETadl007 <ETadl007@gmail.com>
2023-09-08 11:21:39 +0800

    first commit

--- venv/test.py
+++ venv/test.py
@@ -1,7 +1,7 @@
 year = int(input("请输入年份："))
 if ( year % 4 == 0 and year % 100 != 0 or year % 400 == 0) :
-    print("是闰年")
+    print(year,"是闰年")
 else:
-    print("不是")
+    print(year,"不是闰年")
 
 
