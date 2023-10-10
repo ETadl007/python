3de59828e5d96e12bbf637335c445918bb34c1bf
ETadl007 <ETadl007@gmail.com>
2023-10-10 13:46:01 +0800

    first commit

--- venv/练习/demo.py
+++ venv/练习/demo.py
@@ -41,6 +41,12 @@ def is_user_exists(user_val):
     return False
 
 
+def is_exit(is_val):
+    if is_val == 'q':
+        return False
+    return True
+
+
 def store_user_info(user_info):
     with open("account.txt", "a", encoding="utf-8") as f:
         f.write(str(user_info) + "\n")
@@ -50,23 +56,27 @@ def store_user_info(user_info):
 
 
 def register():
-    print("欢迎来到注册程序")
-
-    print("用户名只能包含大小写字母和数字，且不能以数字开头")
+    print("欢迎来到注册程序 --->如不注册请输入q退出")
+    print("**用户名只能包含大小写字母和数字，且不能以数字开头")
     username = input("请输入用户名: ").strip()
+    if not is_exit(username):
+        return None
     if not is_val_username(username):
         return None
 
     print("密码长度必须在6~15位之间")
     password = input("请输入密码: ").strip()
+    if not is_exit(password):
+        return None
     if not is_val_password(password):
         return None
 
     print("按照标准的国内手机号码规则进行校验")
     phone = input("请输入手机号: ").strip()
+    if not is_exit(phone):
+        return None
     if not is_val_phone_number(phone):
         return None
-
     user_info = {
         "username": username,
         "password": password,
@@ -83,9 +93,13 @@ def register():
 
 
 def login():
-    print("欢迎进入登录程序")
-    username = input("请输入用户名: ")
-    password = input("请输入密码: ")
+    print("欢迎进入登录程序--->如不注册请输入q退出")
+    username = input("请输入用户名: ").strip()
+    if not is_exit(username):
+        return None
+    password = input("请输入密码: ").strip()
+    if not is_exit(password):
+        return None
     with open("account.txt", "r", encoding="utf-8") as f:
         lines = f.readlines()
 
